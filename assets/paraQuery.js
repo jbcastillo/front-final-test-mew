fd752b52e8b28161017ab7a0e843c5b3d58ceb49
Jose Rios <lithium@hotmail.es>
2020-03-10 01:15:52 -0300

    en proceso de cambio, ya que cree otra rama que no pude subirla al repositorio

--- assets/paraQuery.js
+++ assets/paraQuery.js
@@ -1,25 +1,29 @@
 const URL1 = 'https://pokeapi.co/api/v2/pokemon/'
-const URL2 = 'https://pokeapi.co/api/v2/pokemon/1/'
-const URL3 = 'https://pokeapi.co/api/v2/evolution-chain/1/'
 var URLaux = ''
 let photo 
-let aux = ''
-let arregloLista = ''
+let auxCartas = ''
+let auxLista = ''
+let arregloLista = []
+let arregloCartas = []
+let datosTxT = ''
 
 $(document).ready(() => {
     $('#seleccion_inicial').change(seleccion)
     seleccion()
 })
 
-
+//busca la opcion ingresada inicial, agregando la pagina: esperandoOpcion.html que solo imprime un parrafo indicando que seleccione una opcion
 function seleccion(){
     var info = $('#seleccion_inicial').get(0)
     var archivo = ''
     switch (info.value) {
-        case "0": archivo = 'pokemon.html'
-            break;
-        case "1": archivo = 'listaPokemon.html'
-            break;
+        case "0": archivo = 'esperandoOpcion.html'
+            break
+        case "1": archivo = 'todosLosPokemon.html'
+            break
+        case "2": archivo = 'listaPokemon.html'
+            break
+        default: archivo = 'esperandoOpcion.html'
     }
     $.ajax({
         "url": archivo,
@@ -29,85 +33,62 @@ function seleccion(){
     })
 }
 
-function cargarPokemon(){
-    let datosTxT
-    let texto2 
+//Esta  funcion se ejecuta desde el archivo todosLosPokemon, en donde existe una division con id del tipo columnas, 
+//todos los valores adquiridos de la URL1, ingresaran a la funcion arregloURL que generara un String con la suma de
+//todas las URL y el nombre adquirido por el fetch. Ejemplo: https://pokeapi.co/api/v2/pokemon/nombre1 ...
+//posterior a esto, el string se guarda en un arreglo llamado arregloCartas y se le elimina el primer elemento que 
+//es undefined, con la funcion.shift(). Teniendo todas las URL de cada pokemon en este arreglo, se recorrera para 
+//consumir los datos necesarios de cada URL creada.
+function cartasPokemon() {
     fetch(URL1, { method: 'GET' })
         .then(x => x.json())
-        .then(x => x.results.forEach(pokemon => imprime_pokemon(pokemon)))
+        .then(x => x.results.forEach(pokemon => arregloURL(pokemon)))
 
-    const imprime_pokemon = (elemento) => {
-        texto2 += ` ` 
-        URLaux = URL1+elemento.name
-        fetch(URLaux, {method: 'GET'})
-            .then(x => x.json())
-            .then(pokemon => imprime(pokemon))
-
-            const imprime = (poke) => {
-                datosTxT += ` 
-                <td>
-                    <div class="card" style="width: 18rem;">
-                        <img src="${poke.sprites.front_default}" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title text-black-50">${poke.name}</h5>
-                        <h5 class="card-title text-black-50">${poke.id}</h5>
-                        </div>
-                    </div>
-                    </td>
-                `
-                document.getElementById("cuerpo_tabla").innerHTML = datosTxT
-            }
+    const arregloURL = (elemento) => {
+        auxCartas += ";" + URL1 + elemento.name
     }
+    arregloCartas = auxCartas.split(';')
+    arregloCartas.shift()
+    // console.log(arregloLista.length)
+    arregloCartas.forEach(x => promesa(x))
 }
 
-function listarPokemon() {
-    let datosTxT
+function promesa(url){
+    fetch(url, { method: 'GET'})
+    .then(x => x.json())
+            .then(pokemon => imprime(pokemon))
+}
 
+function imprime(poke){
+    datosTxT += ` 
+    <div class="col mb-4">
+        <div class="card">
+        <img src="${poke.sprites.front_default}" class="card-img-top" alt="${poke.name}">
+            <div class="card-body">
+                <p class="card-text"><small class="text-muted">Nombre Pokemon:</small></p>
+                <h5 class="card-title text-black-50">${poke.name}</h5>
+                <p class="card-text text-black-50">ID: ${poke.id}</p>
+            </div>
+        </div>
+    </div>
+    `
+    document.getElementById("cuerpo_carta").innerHTML = datosTxT
+}
+
+//Esta  funcion se ejecuta desde el archivo listarPokemon, en donde existe un selector con id el cual se sobre escribira con
+//todos los valores adquiridos de la URL1, para posteriormente poder acceder a cada pokemon de forma individual
+function listarPokemon() {
     fetch(URL1, { method: 'GET' })
         .then(x => x.json())
         .then(x => x.results.forEach(pokemon => listar(pokemon)))
 
     const listar = (elemento) => {
-        aux += ";" + URL1 + elemento.name
+        auxLista += ";" + URL1 + elemento.name
         datosTxT += ` 
                     <option>${elemento.name}</option>
                 `
         document.getElementById("info2").innerHTML = datosTxT
     }
-    arregloLista = aux.split(';')
-    imprimirLink()
-}
-
-
-function imprimirLink(){
-    console.log(arregloLista)
-}
-function cargarHabilidad(){
-    datosTxT = null
-
-    fetch(URL2, { method: 'GET' })
-        .then(x => x.json())
-        .then(x => x.abilities.forEach(pokemon => imprime_pokemon(pokemon)))
-
-    const imprime_pokemon = (elemento) => {
-        datosTxT += ` 
-    <tr>
-        <td>${elemento.ability.name}</td>
-    </tr>
-    `
-        document.getElementById("cuerpo_tabla").innerHTML = datosTxT
-    }
-}
-
-function cargarEvoluciones(){
-    datosTxT = null
-
-    fetch(URL3, { method: 'GET' })
-        .then(x => x.json())
-        .then(x => x.chain.evolves_to.forEach(pokemon => imprime_pokemon(pokemon)))
-
-    const imprime_pokemon = (elemento) => {
-        datosTxT += `<tr><a href="https://pokeapi.co/api/v2/pokemon-species/2/"> ${elemento.species.name} </a></tr>`
-        document.getElementById("cuerpo_tabla").innerHTML = datosTxT
-    }
+    arregloLista = auxLista.split(';')
+    arregloLista.shift()
 }
